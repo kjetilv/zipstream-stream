fcd9b0e62b1ae49dc485186264e61694ee2926cb
Kjetil Valstadsve <valstadsve@gmail.com>
2017-10-09 08:47:04 +0200

    Make work.

--- src/main/java/no/scienta/alchemy/zipstreamstream/ZipStreamStream.java
+++ src/main/java/no/scienta/alchemy/zipstreamstream/ZipStreamStream.java
@@ -1,10 +1,15 @@
 package no.scienta.alchemy.zipstreamstream;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.Spliterator;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
@@ -70,5 +75,132 @@ public class ZipStreamStream {
 
     private static Stream<ZipEntry> entryStream(ZipInputStream zis) {
         return StreamSupport.stream(new ZipEntrySpliterator(zis), false);
+    }
+
+    private static class ZipEntrySpliterator implements Spliterator<ZipEntry> {
+
+        private final AtomicReference<ZipEntry> current;
+
+        private final ZipInputStream zis;
+
+        ZipEntrySpliterator(ZipInputStream zis) {
+            this.zis = zis;
+            current = new AtomicReference<>();
+        }
+
+        @Override
+        public boolean tryAdvance(Consumer<? super ZipEntry> action) {
+            closePreviousEntry();
+            ZipEntry nextEntry = nextEntry();
+            return nextEntry == null
+                    ? finishStream()
+                    : processNext(action, nextEntry);
+        }
+
+        @Override
+        public Spliterator<ZipEntry> trySplit() {
+            return null;
+        }
+
+        @Override
+        public long estimateSize() {
+            return Long.MAX_VALUE;
+        }
+
+        @Override
+        public int characteristics() {
+            return Spliterator.NONNULL & Spliterator.IMMUTABLE;
+        }
+
+        private boolean processNext(Consumer<? super ZipEntry> action, ZipEntry nextEntry) {
+            try {
+                action.accept(nextEntry);
+            } finally {
+                current.set(nextEntry);
+            }
+            return true;
+        }
+
+        private boolean finishStream() {
+            try {
+                zis.close();
+            } catch (IOException e) {
+                throw new IllegalStateException(e);
+            }
+            return false;
+        }
+
+        private void closePreviousEntry() {
+            Optional.ofNullable(current.get()).ifPresent(entry -> {
+                try {
+                    zis.closeEntry();
+                } catch (IOException e) {
+                    throw new IllegalStateException(e);
+                }
+            });
+        }
+
+        private ZipEntry nextEntry() {
+            ZipEntry nextEntry;
+            try {
+                nextEntry = zis.getNextEntry();
+            } catch (IOException e) {
+                throw new IllegalStateException(e);
+            }
+            return nextEntry;
+        }
+    }
+
+    private static final class UncloseableInputStream extends InputStream {
+
+        private final InputStream inputStream;
+
+        UncloseableInputStream(InputStream inputStream) {
+            this.inputStream = inputStream;
+        }
+
+        @Override
+        public void close() throws IOException {
+        }
+
+        @Override
+        public int read(byte[] b) throws IOException {
+            return inputStream.read(b);
+        }
+
+        @Override
+        public int read(byte[] b, int off, int len) throws IOException {
+            return inputStream.read(b, off, len);
+        }
+
+        @Override
+        public long skip(long n) throws IOException {
+            return inputStream.skip(n);
+        }
+
+        @Override
+        public int available() throws IOException {
+            return inputStream.available();
+        }
+
+        @Override
+        public synchronized void mark(int readlimit) {
+            inputStream.mark(readlimit);
+        }
+
+        @Override
+        public synchronized void reset() throws IOException {
+            inputStream.reset();
+        }
+
+        @Override
+        public boolean markSupported() {
+            return inputStream.markSupported();
+        }
+
+        @Override
+        public int read() throws IOException {
+            return inputStream.read();
+        }
     }
 }
